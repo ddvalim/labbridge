79948d2cbae0f725d7773e293f8af5b57ec3c63f
Diovana <diovanavalimm@gmail.com>
2020-09-15 22:16:44 -0300

    acrescentado historico

--- src/Components/Body/Resolução/index.js
+++ src/Components/Body/Resolução/index.js
@@ -8,9 +8,20 @@ const Resolucao = () => {
     const [value, setValue] = useState("")
     
     const req = async () => {
-        let a = await api.get(`/api/calculator/${value}`)
-        setPadrao(a.data)
+        let res = await api.get(`/api/calculator/${value}`)
+        setPadrao(res.data)
     } 
+
+    const [Map, setMap] = useState("")
+
+    setInterval(async () => {
+        let res = await api.get(`/api/calculator/historic`)
+        setMap(res.data)
+    }, 3000)
+    
+    const number = Object.keys(Map)
+    const fac = Object.values(Map)
+
     return (
     <div>
         <fieldset id="fieldset">
@@ -18,6 +29,30 @@ const Resolucao = () => {
             <input onChange={e => setValue(e.target.value)} id="numero"></input><button onClick={req} id="button">Enviar</button>
             <div id='resultado'>
                 {padrao ? padrao:""}
+            </div>
+            <div id='historico'>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>
+                                Number
+                            </th>
+                            <th>
+                                Result
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div> { number } </div><br></br>
+                            </td>
+                            <td>
+                                <div> { fac } </div><br></br>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </fieldset>
     </div>
